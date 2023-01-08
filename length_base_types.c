f15decfdf818b66aca8961fe849d9bd2f0dc159f
zip1982b <zip1982b@gmail.com>
2023-01-08 06:22:20 +0200

    узнаём размеры в байтах базовых типов данных на конкретных машинах

---
+++ length_base_types.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+	printf("Hello World\n");
+    printf("тип int = %ld байтов, в данной машине\n", sizeof(int));
+    printf("тип float = %ld байтов, в данной машине\n", sizeof(float));
+    printf("тип char = %ld байтов, в данной машине\n", sizeof(char));
+
+    return 0;
+}
+
